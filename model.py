85f81bc5aa5f39d54dce1e16081315be02dd6e85
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-05-07 01:43:54 -0700

    added Flask

--- model.py
+++ model.py
@@ -6,6 +6,10 @@ import sys
 import io
 import PIL
 
+model_path = 'models/'
+output_path = 'predictions/'
+label_dict = {0: 'NORMAL', 1: 'PNEUMONIA'}
+learner = load_learner(path = model_path, file = 'dense')
 
 def image_from_bytes(image_bytes):
     im = PIL.Image.open(io.BytesIO(image_bytes))
@@ -13,34 +17,27 @@ def image_from_bytes(image_bytes):
     im = Image(im)
     return im
 
-def predict(im, p_name = 'prediction.png'):
-    model_path = 'models/'
-    output_path = 'predictions/'
-    label_dict = {0: 'NORMAL', 1: 'PNEUMONIA'}
-    learn = load_learner(path = model_path, file = 'dense')
-    fig = plot_class_pred(learn, im, label_dict)
-    fig.savefig(output_path + p_name, bbox_inches='tight')
-
-
-
-def predict_from_path(im_path):
-    im = open_image(im_path)
-    predict(im, im_path.split('/')[-1])
-
-def plot_class_pred(learner, im, label_dict):
-
+def predict(im):
     category, category_t, preds = learner.predict(im)
+    return int(category_t), preds
+
+def fig_from_pred(im, category_t, preds):
     fig = plt.figure(1)
     plt.imshow(im.data.detach().numpy().transpose(1, 2, 0))
     plt.gca().set_title('{0}, {1:.1f}%'.format(
-        label_dict[int(category_t)],
-        preds[int(category_t)].detach().numpy() * 100),
+        label_dict[category_t],
+        preds[category_t].detach().numpy() * 100),
         color=("green")
     )
-
     plt.axis('off')
     return fig
 
+def predict_from_path(im_path, savefig = True):
+    im = open_image(im_path)
+    category_t, preds = predict(im)
+    if savefig:
+        fig = fig_from_pred(im, category_t, preds)
+        fig.savefig(output_path + im_path.split('/')[-1], bbox_inches='tight')
 
 
 if __name__ == '__main__':
