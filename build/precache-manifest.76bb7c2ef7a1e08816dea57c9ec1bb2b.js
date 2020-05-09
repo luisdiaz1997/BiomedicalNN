83996e27c3bb6b5fc40949116a92faab0c0920c6
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-05-08 23:47:01 -0700

    idk

---
+++ build/precache-manifest.76bb7c2ef7a1e08816dea57c9ec1bb2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b686427f5479089f98046ab3f3deb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28519667cbc8516d39f8",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "44d1c83e738cf96d65d3",
+    "url": "/static/css/main.5d4eb1f5.chunk.css"
+  },
+  {
+    "revision": "28519667cbc8516d39f8",
+    "url": "/static/js/2.2243a24c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2243a24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d1c83e738cf96d65d3",
+    "url": "/static/js/main.954e962d.chunk.js"
+  },
+  {
+    "revision": "9cb16b117985f0abc63b",
+    "url": "/static/js/runtime-main.60d3f0ab.js"
+  }
+]);
